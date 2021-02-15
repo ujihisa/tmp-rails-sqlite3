859ea4fe4abdc651b8a2b67d1fc2c56ab3f3c47b
ujihisa <ujihisa@users.noreply.github.com>
2021-02-15 10:29:20 -0800

    Get migrated

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,25 @@
+## download ./db/production.sqlite3
+require 'google/cloud/storage'
+
+credentials =
+  if ENV['CREDENTIALS_JSON']
+    JSON.pasre(ENV['CREDENTIALS_JSON'])
+  else
+    'devs-sandbox-5941dd8999bb.json'
+  end
+storage = Google::Cloud::Storage.new(
+  project_id: 'devs-sandbox',
+  credentials: credentials,
+)
+
+bucket = storage.bucket('tmp-rails-sqlite3')
+file = bucket.file('production.sqlite3')
+file.download('db/production.sqlite3')
+
+## ok
+
+
+
 require_relative 'boot'
 
 require 'rails/all'
