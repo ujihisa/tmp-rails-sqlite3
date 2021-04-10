07bd0a3ba0b1a8b7977bf12b83fc579cd6842eda
ujihisa <ujihisa@users.noreply.github.com>
2021-04-09 20:22:20 -0700

    Dump debug message

--- start_app.sh
+++ start_app.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -eu
-# echo "GOOGLE_APPLICATION_CREDENTIALS: ${GOOGLE_APPLICATION_CREDENTIALS}"
-gcsfuse tmp-rails-sqlite3 ./tmp/aaa
+if [ -v GOOGLE_APPLICATION_CREDENTIALS ]; then
+  echo "GOOGLE_APPLICATION_CREDENTIALS: ${GOOGLE_APPLICATION_CREDENTIALS}"
+  gcsfuse tmp-rails-sqlite3 ./tmp/aaa
+else
+  echo "GOOGLE_APPLICATION_CREDENTIALS not defined"
+  env
+fi
+
 ls -a
 bin/rails server --binding 0.0.0.0
